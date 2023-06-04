eac4032783b2c21403d2902e02dc43e782a055d7
Alexandre Almeida <alexandrebrcn@gmail.com>
2023-06-04 10:54:55 +0100

    added show to routes, set show#posts and created view show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  get "/posts", to: "posts#index"
+  resources :posts, only: %i[index show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
