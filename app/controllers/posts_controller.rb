8faad12dc2f6db64198ece5337173a9975af2dbf
Alexandre Almeida <alexandrebrcn@gmail.com>
2023-06-04 11:15:30 +0100

    added update routes, included edit and update functions on post controller, applied DRY to post simple form, changed new.html.erb, added edit.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,19 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  resources :posts, only: %i[index new create show]
+  resources :posts, only: %i[index new create show edit]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
