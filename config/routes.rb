084fcedc78036d33bfb00b5b815a2825e783318c
Alexandre Almeida <alexandrebrcn@gmail.com>
2023-06-04 13:11:56 +0100

    created model reviews, set associations on models, create route new nested on posts, create action new and create on controller, added view new

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   root to: "posts#index"
-  resources :posts, only: %i[index new create show edit]
+  resources :posts, only: %i[index new create show edit update] do
+    resources :reviews, only: %i[new create]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
