959b8f4d2cc7470c9af8c0cd3e255bd9a62538d2
Alexandre Almeida <alexandrebrcn@gmail.com>
2023-06-04 11:05:40 +0100

    created new and create function on post controller, set view for new post, added strong parametes, routes for new and create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  resources :posts, only: %i[index show]
+  resources :posts, only: %i[index new create show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
