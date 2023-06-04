a1d36d73b36dde56712574027cf9cf6da189b9e3
Alexandre Almeida <alexandrebrcn@gmail.com>
2023-06-04 10:41:57 +0100

    created post model, generated db, add faker gem add seeds to post

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Dropping data..."
+Post.destroy_all
+puts "Creating new seed"
+5.times do | index |
+  puts "creating #{index + 1} Post"
+  Post.create(title: Faker::Book.title, content: Faker::Lorem.paragraph)
+  puts "#{index + 1} Post created!"
+end
