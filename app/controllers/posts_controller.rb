943d26e8e2ac710e676ad3c8c5c0c19be8015f53
Alexandre Almeida <alexandrebrcn@gmail.com>
2023-06-04 13:32:18 +0100

    New seeding final touch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,6 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
-
   def new
     @post = Post.new
   end
@@ -18,6 +14,10 @@ class PostsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def edit
