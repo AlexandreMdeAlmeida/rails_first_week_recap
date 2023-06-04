19b3085641f9a6258c1e91e57628806f0a84dab9
Alexandre Almeida <alexandrebrcn@gmail.com>
2023-06-04 10:50:39 +0100

    created posts controller, add index to routes, set index#posts to root, created index posts file added index.sccs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: "posts#index"
+  get "/posts", to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
